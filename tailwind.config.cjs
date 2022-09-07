393daa9ad65041657632804d870ffc3a02c0b39f
Michael Owuor <michael.owuor@gmail.com>
2022-09-07 11:38:16 +0300

    chore: modify tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	content: ['./index.html'],
 	theme: {
 		fontFamily: {
 			ubuntu: 'Ubuntu, sans-serif',
